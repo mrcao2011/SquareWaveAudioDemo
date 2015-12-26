7d35f77f82a7ef4ec4a30c7e3701c1ceeb9cfc39
mr cao <mrcao@mrcaodeimac-5963.local>
2015-12-26 16:00:03 +0800

    Initial Commit

---
+++ SquareWaveAudioDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SquareWaveAudioDemo
+//
+//  Created by mr.cao on 15/12/26.
+//  Copyright © 2015年 mr.cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SquareWaveAudioDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SquareWaveAudioDemo
+//
+//  Created by mr.cao on 15/12/26.
+//  Copyright © 2015年 mr.cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
